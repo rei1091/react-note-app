d9ee6f4edff5ef59d849303cf37106cbec7dad47
rei1091 <rei.bb.1091@gmail>
2023-09-20 13:36:12 +0900

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Main.css';
+import {ReactMarkdown} from 'react-markdown/lib/react-markdown';
+
+function Main({activeNote,onUpdateNote}) {
+	const onEditNote = (key, value) => {
+		onUpdateNote({
+		...activeNote,
+			[key]: value,
+			modDate: Date.now()
+		});
+	};
+
+	if(!activeNote){
+		return <div className='no-active-note'>
+			ノートが選択されてません</div>
+	}
+	return (
+		<div className='app-main'>
+			<div className='app-main-note-edit'>
+				<input
+				id='title'
+				type='text' placeholder='Title'
+				value={activeNote.title}
+				onChange={(e) =>
+				onEditNote('title', e.target.value)}/>
+				<textarea id=''
+				placeholder='ノート内容記入欄'
+				value={activeNote.content}
+				onChange={(e) =>
+					onEditNote('content', e.target.value)}
+					></textarea>
+			</div>
+			<div className='app-main-note-preview'>
+				<h1 className='preview-title'>
+					{activeNote.title}
+					</h1>
+				<ReactMarkdown className='markdown-preview'>
+					{activeNote.content}
+				</ReactMarkdown>
+			</div>
+		</div>
+	)
+}
+
+export default Main
